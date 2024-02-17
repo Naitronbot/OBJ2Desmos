21e40e790d5f21738858c137e037f57c25253210
Naitronbot <lackpup@gmail.com>
2024-02-16 19:44:54 -0800

    Add drag and drop to input

--- src/DataInput/DataInput.svelte
+++ src/DataInput/DataInput.svelte
@@ -5,6 +5,25 @@
     export let name: string;
     export let filetype: string;
     let fileInput: HTMLInputElement;
+    let dragging: "dragging" | "" = "";
+
+    async function droppedImage(e: DragEvent) {
+        e.preventDefault();
+        
+        const files = e.dataTransfer?.items;
+        if (files) {
+            const fileText = await [...files]
+                .find(file => file.kind === "file")
+                ?.getAsFile()
+                ?.text();
+
+            if (fileText) {
+                value = fileText;
+            }
+        }
+
+        dragging = "";
+    }
 
     async function handleUpload() {
         const file = fileInput.files?.item(0);
@@ -18,7 +37,19 @@
     <div class="title-wrapper">
         <h1>{name} Data</h1>
     </div>
-    <textarea bind:value></textarea>
-    <button on:click={() => fileInput.click()}>Upload {name} <img src="/src/assets/upload.svg" alt="Upload"/></button>
+    <textarea
+        class={dragging}
+        on:drop={droppedImage}
+        on:dragover={(e) => e.preventDefault()}
+        on:dragenter={() => dragging = "dragging"}
+        on:dragleave={() => dragging = ""}
+        bind:value
+    ></textarea>
+    <button
+        on:click={() => fileInput.click()}
+        on:drop={droppedImage}
+        on:dragover={(e) => e.preventDefault()}
+        >Upload {name} <img src="/src/assets/upload.svg" alt="Upload"
+    /></button>
     <input on:change={handleUpload} accept={"."+filetype} type="file" bind:this={fileInput} hidden>
 </div>
